1b653428eedf715186e884ac831d17c31a066043
Ctmax-ui <debjeetbiswas121@gmail.com>
2024-09-12 13:58:39 +0530

    fixed npm run dev big that needs an npm command

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+        <script src="https://cdn.tailwindcss.com"></script>
+        </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
